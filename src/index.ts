8e9e8b57d91bbb3c6a2e61a9f041f89d2c240bd0
Katherine Martinez <katherine.martinez@coinbase.com>
2020-12-31 08:59:25 -0500

    Fix input types

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
-import { parse } from '@babel/parser';
-import { ObjectProperty, SpreadElement, ObjectMethod } from '@babel/types';
-import { PaletteAlias, PaletteConfig, CssVariable, AnyObject } from '@kmart/types';
+import { parse } from "@babel/parser";
+import { ObjectProperty, SpreadElement, ObjectMethod } from "@babel/types";
+import {
+  PaletteAlias,
+  PaletteConfig,
+  CssVariable,
+  AnyObject,
+} from "@kmart/types";
 import { toCssVar } from "@kmart/utils";
-import { createMacro } from 'babel-plugin-macros';
+import { createMacro } from "babel-plugin-macros";
 
 export type MacroHandler = Parameters<typeof createMacro>[0];
 
 const convertPalette = (nodes: ObjectProperty[]) => {
   return nodes.reduce((prev, { key, value }) => {
-    const name = key.type === 'Identifier' ?  toCssVar(key.name) as PaletteAlias : '';
+    const name =
+      key.type === "Identifier"
+        ? (toCssVar(key.name) as CssVariable<PaletteAlias>)
+        : "";
     let result;
-    if (value.type === 'ArrayExpression') {
-      const elements = (value.elements as unknown) as [{ value: PaletteAlias }, { value: string }];
-      const [alias, opacity] = elements.map(item => item.value);
+    if (value.type === "ArrayExpression") {
+      const elements = (value.elements as unknown) as [
+        { value: PaletteAlias },
+        { value: string }
+      ];
+      const [alias, opacity] = elements.map((item) => item.value);
       result = `rgba(var(--${alias}), ${opacity})`;
-    } else if (value.type === 'StringLiteral') {
+    } else if (value.type === "StringLiteral") {
       const alias = (value.value as unknown) as PaletteAlias;
       result = `rgb(var(--${alias}))`;
     }
@@ -30,28 +41,32 @@ const convertPalette = (nodes: ObjectProperty[]) => {
   }, {} as Record<CssVariable<PaletteAlias>, string>);
 };
 
-const filterProperties = <T extends (ObjectMethod | ObjectProperty | SpreadElement)[]>(
+const filterProperties = <
+  T extends (ObjectMethod | ObjectProperty | SpreadElement)[]
+>(
   props: T
 ) => {
-  return props.filter(item => item.type === 'ObjectProperty') as ObjectProperty[];
+  return props.filter(
+    (item) => item.type === "ObjectProperty"
+  ) as ObjectProperty[];
 };
 
 const objectToAst = <T extends AnyObject>(obj: T) => {
   const result = parse(`const temp = ${JSON.stringify(obj)}`);
   if (result) {
     const body = result.program.body[0];
-    if (body.type === 'VariableDeclaration') {
+    if (body.type === "VariableDeclaration") {
       return body.declarations[0].init;
     }
   }
 };
 
 const createPalette: MacroHandler = ({ references }) => {
-  references.default.forEach(referencePath => {
-    const args = referencePath.parentPath.get('arguments');
+  references.default.forEach((referencePath) => {
+    const args = referencePath.parentPath.get("arguments");
     if (Array.isArray(args)) {
       const [firstArgumentPath] = args;
-      if (firstArgumentPath.node.type === 'ObjectExpression') {
+      if (firstArgumentPath.node.type === "ObjectExpression") {
         const properties = filterProperties(firstArgumentPath.node.properties);
         const palette = convertPalette(properties);
         const paletteInAst = objectToAst(palette);
@@ -64,5 +79,5 @@ const createPalette: MacroHandler = ({ references }) => {
 };
 
 export default createMacro(createPalette) as (
-  config: PaletteConfig
+  config: Partial<PaletteConfig>
 ) => Record<CssVariable<PaletteAlias>, string>;
